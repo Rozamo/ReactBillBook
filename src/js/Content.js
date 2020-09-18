672a30a1da4a7d81384af67441fb8aac9600a6ab
ShivamRZP <shivam.murmu@razorpay.com>
2020-09-18 22:29:29 +0530

    Added state

--- src/js/Content.js
+++ src/js/Content.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import SideBarItem from './SideBar/SideBarItem';
+import SidebarItem from './SideBar/SidebarItem';
 import CustomersTable from './Customers/CustomersTable';
 import CustomersForm from './Customers/CustomersForm';
 import ItemsTable from './Items/ItemsTable';
 import ItemsForm from './Items/ItemsForm';
 
 export default function Content() {
+    const [sidebarChoice, setSidebarChoice] = useState('');
+    function changeSidebarChoice(sidebarChoice) {
+        setSidebarChoice(sidebarChoice);
+    }
+    useEffect(() => {
+        console.log(sidebarChoice);
+    });
     return <Router>
         <div className="outside-box">
             <div className="side-bar">
                 <Link to='/customers/list'>
-                    <SideBarItem value="Customers" sidebar_choice="customers" cls="customers"/>
+                    <SidebarItem value="Customers" sidebarChoice="customers" cls={"customers".concat(sidebarChoice === "customers" ? " active" : "")} changeSidebarChoice={changeSidebarChoice}/>
                 </Link>
                 <Link to='/items/list'>
-                    <SideBarItem value="Items" sidebar_choice="items" cls="items"/>
+                    <SidebarItem value="Items" sidebarChoice="items" cls={"items".concat(sidebarChoice === "items" ? " active" : "")} changeSidebarChoice={changeSidebarChoice}/>
                 </Link>
             </div>
             <Switch>
                 <Route exact path='/' component={() => <div className="content"></div>}/>
-                <Route exact path='/customers/list' component={CustomersTable}/>
-                <Route exact path='/customers/create' component={CustomersForm}/>
-                <Route exact path='/items/list' exact component={ItemsTable}/>
-                <Route exact path='/items/create' exact component={ItemsForm}/>
+                <Route path='/customers/list' component={CustomersTable}/>
+                <Route path='/customers/create' component={CustomersForm}/>
+                <Route path='/items/list' exact component={ItemsTable}/>
+                <Route path='/items/create' exact component={ItemsForm}/>
             </Switch>
         </div>
     </Router>;
